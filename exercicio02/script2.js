53a6a025819382aed3cd6e7478b6cf8f076f7766
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-11-04 14:55:12 -0300

    upload do projeto

--- exercicio02/script2.js
+++ exercicio02/script2.js
@@ -1,6 +1,6 @@
 let altura = 0;
-let maior_altura = 0;
-let menor_altura = 0;
+let maior_altura = Number.MAX_VALUE;
+let menor_altura = Number.MIN_VALUE;
 
 for (let i = 1; i < 16; i++) {
     altura = +prompt("Digite sua altura, usando . para separar")
---
+++ funcoes/funcoes.js
@@ -0,0 +1,7 @@
+function perguntaNomeMostraNome() {
+    let nome = prompt("Digite o seu nome");
+    alert("Seu nome é: " + nome);
+}
+function chamarjoao() {
+    alert("João");
+}
