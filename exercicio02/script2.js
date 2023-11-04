409cd8e7d36f6b581ac0ecfdf476265f1805bf28
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-11-04 13:49:12 -0300

    upload do projeto

--- exercicio02/script2.js
+++ exercicio02/script2.js
@@ -1,3 +1,16 @@
-for(let i = 1;i <16;i++){
+let altura = 0;
+let maior_altura = 0;
+let menor_altura = 0;
 
+for (let i = 1; i < 16; i++) {
+    altura = +prompt("Digite sua altura, usando . para separar")
+    if (altura > maior_altura) {
+        maior_altura = altura
+    }
+    if (altura > menor_altura) {
+        menor_altura = altura
+    }
 }
+
+alert(maior_altura)
+alert(menor_altura)
