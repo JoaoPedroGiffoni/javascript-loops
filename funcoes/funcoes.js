d4ecb40e9d0620c34ea229483ba73331b9ed8e10
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-11-04 16:21:01 -0300

    upload do projeto

--- funcoes/funcoes.js
+++ funcoes/funcoes.js
@@ -5,3 +5,26 @@ function perguntaNomeMostraNome() {
 function chamarjoao() {
     alert("João");
 }
+function dizUmNome() {
+    let nome = "Ronier";
+    return nome;
+}
+function pi() {
+    return 3.14;
+}
+
+// function quadrado(lado) {
+//     alert(lado)
+//     return lado * lado;
+// }
+// let retorno = quadrado(2);
+// alert(retorno)
+
+function multi5(numero) {
+    return numero * 5
+}
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+alert(soma(5,2))
